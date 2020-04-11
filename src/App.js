ab0095f39320fc840aed643ac3d2da8b88cc91fa
Pedro Amaral <pedrotaem@gmail.com>
2020-04-11 12:47:36 -0300

    chore: First challange!

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import backgroundImage from './assets/background.jpg';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+      console.log(response);
+    })
+  }, []);
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: "https://github.com/pedrotaem/Desafio_1-_Conceitos_do_NodeJS",
+      techs: [ "Javascript"]	
+    })
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    console.log(repositories);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+    const repositoryIndex = [...repositories].findIndex((repository) => repository.id === id);
+    const repositories_total = [...repositories];
+    repositories_total.splice(repositoryIndex,1);
+    setRepositories(repositories_total);
   }
 
   return (
     <div>
+      <Header title="Projetos - Repos"/>
+      <img width={500} src={backgroundImage} />
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => <li key ={repository.id}>{repository.title}
+            <button type="button" onClick={() => handleRemoveRepository(repository.id)}>
             Remover
-          </button>
-        </li>
+            </button></li>)}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button type="button" onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
